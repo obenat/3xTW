a41292a077a2ddd1c591b50a9e880786d59ca67f
Git_Goat <stpicnoa@bx.fallmerayer.it>
2022-02-13 22:53:15 +0100

    fostfertig

--- 1/firstTest/script.js
+++ 1/firstTest/script.js
@@ -132,7 +132,7 @@ function getStations(data)//fill and put all station objects into array
 function getTransportationRoute(data)//make object for every single route
 {
   let routes = [];
-  if(data.points != null)
+  if(data.trips != null)
     data.trips.forEach(x => 
     {
       routes.push({totalDuration: x.duration, stations: getStations(x)});
@@ -153,25 +153,27 @@ function stopFinder(location)
 //-----------------------------------------------------------------------------------------------------------------------------------------------------
 async function searchRoute(location1, location2)
 {
-  /*let testlocation = "Brixen - Vahrn";
+  let testlocation = "Brixen - Vahrn";
   let testlocation1 = "Brixen";
   let teststation1 = "Dantestraße";
   let testlocation2 = "Klausen";
-  let teststation2 = "Bahnhof";*/
+  let teststation2 = "Bahnhof";/*
   let testlocation = "asdf";
   let testlocation1 = "ddddddddd";
   let teststation1 = "ffffffff";
   let testlocation2 = "Boazen";
-  let teststation2 = "Ortler";
-
-  //let testData1 = getTransportationRoute(await getDataFromURL(getEFARouteURL(testlocation1, teststation1, testlocation2, teststation2)));
+  let teststation2 = "Ortler";*/
+  //var clone=document.getElementById("routeDiv").cloneNode();
+  let testData1 = getTransportationRoute(await getDataFromURL(getEFARouteURL(testlocation1, teststation1, testlocation2, teststation2)));
   let testData2 = getmotFromStation(await getDataFromURL(getEFAStationURL(testlocation1, teststation1)));
-  //let testData3 = await getWeather(testlocation);
+  let testData3 = await getWeather(testlocation);
   /*console.log(testData1);
   console.log(testData2);
   console.log(testData3);*/
   //let asdf = stopFinder(await getDataFromURL(getStopFinderURL("Bx")));
-  console.log(testData2);
+  console.log(testData1);
+  displayroute(testData1);
+  displayweather(testData3);
   //console.log(testData3.iconCode);
   /*console.log(testData3);*/
 
@@ -181,6 +183,147 @@ async function searchRoute(location1, location2)
   console.log(await getDataFromURL(getEFAStationURL(location1, station1)));
   console.log(await getDataFromURL(getEfaRouteURL(location1, station1, location2, station2)));*/
 }
+
+
+function displayroute(data){
+
+  for(let i=0;i<data.length;i++){
+    document.getElementsByClassName("names")[i].innerHTML='';
+    document.getElementsByClassName("sep")[i].innerHTML=''
+    document.getElementsByClassName("totp")[i].innerHTML=''
+    document.getElementsByClassName("transport")[i].innerHTML=''
+    document.getElementsByClassName("sloc")[i].innerHTML=''
+    document.getElementsByClassName("middle")[i].innerHTML=''
+    document.getElementsByClassName("destloc")[i].innerHTML=''
+    document.getElementsByClassName("desttime")[i].innerHTML=''
+    document.getElementsByClassName("stime")[i].innerHTML=''
+
+  }
+  document.getElementById("routeDiv").style.display="block";
+  console.log(data[0].stations);
+  for(let i=0;i<data.length;i++){
+    
+      currentDiv=document.getElementById("route"+i);
+      currentStation=data[i].stations;
+      currentDiv.getElementsByClassName("sep")[0].textContent=data[i].totalDuration+"h";
+      currentDiv.getElementsByClassName("totp")[0].textContent=currentStation[0].startTime+" - "+currentStation[currentStation.length-1].endTime;
+      for(let j=0;j<currentStation.length;j++){
+
+        var trans=document.createElement('div');
+        var trsnumb=document.createElement('div');
+        var icon=document.createElement('div');
+        var arrow=document.createElement('div');
+
+        var stloc=document.createElement('div');
+        var destloc=document.createElement('div');
+        var marrow=document.createElement('div');
+        var desttime=document.createElement('div');
+        var stime=document.createElement('div');
+
+        stloc.className="stloc";
+        destloc.className="destloc";
+        marrow.className="marrow";
+
+        icon.innerHTML='<i class="fa-solid fa-train" aria-hidden="true"></i>';
+      
+        if(currentStation[j].transportationType=='Citybus'){
+          icon.innerHTML='<i class="fa-solid fa-bus" aria-hidden="true"></i>';
+        }
+        else if(currentStation[j].transportationType=='Regionalzug'){
+          icon.innerHTML='<i class="fa-solid fa-train" aria-hidden="true"></i>';
+        }
+        else if(currentStation[j].transportationType=='Bus'){
+          icon.innerHTML='<i class="fa-solid fa-bus-simple" aria-hidden="true"></i>';
+        }
+        trsnumb.className="trsnumb1";
+        icon.className="trsnumb2";
+        trans.className="trsnumb";
+        trsnumb.textContent=currentStation[j].transportationNumber;
+        trans.appendChild(trsnumb);
+        trans.appendChild(icon);
+        
+        arrow.innerHTML='<i class="fa-solid fa-angle-right"></i>';
+        arrow.style.color='#a1303f';
+        arrow.className='arrow';
+        currentDiv.getElementsByClassName("names")[0].appendChild(trans);
+        if(j!=currentStation.length-1)
+        currentDiv.getElementsByClassName("names")[0].appendChild(arrow);
+
+        stloc.textContent=currentStation[j].startLocation;
+        destloc.textContent=currentStation[j].endLocation;
+        marrow.textContent=currentStation[j].transportationNumber;
+        desttime.textContent=currentStation[j].endTime;
+        stime.textContent=currentStation[j].startTime;
+        console.log(i)
+
+        currentDiv.getElementsByClassName("sec1")[0].getElementsByClassName("sloc")[0].appendChild(stloc);
+        currentDiv.getElementsByClassName("sec3")[0].getElementsByClassName("destloc")[0].appendChild(destloc);
+        currentDiv.getElementsByClassName("sec2")[0].getElementsByClassName("middle")[0].appendChild(marrow);
+        currentDiv.getElementsByClassName("sec4")[0].getElementsByClassName("desttime")[0].appendChild(desttime);
+        currentDiv.getElementsByClassName("sec5")[0].getElementsByClassName("stime")[0].appendChild(stime);
+
+      }
+
+  }
+  addListeners(data.length);
+}
+
+function addListeners(length){
+  for(let i=0;i<length;i++){
+    const targetDiv = document.getElementsByClassName("sminfo")[i];
+    btn=document.getElementsByClassName("smbtn")[i];
+    btn.onclick=function(){
+      if (targetDiv.style.display !== "none") {
+        targetDiv.style.display = "none";
+        document.getElementsByClassName("sm")[i].className='fa-solid fa-angle-down sm';
+      } else {
+        targetDiv.style.display = "flex";
+        document.getElementsByClassName("sm")[i].className='fa-solid fa-angle-up sm';
+      }
+    };
+  }
+}
+function resetDiv(){
+
+  
+
+}
+
+function displayweather(data){
+  document.getElementById("demoA").textContent='';
+  document.getElementById("demoB").textContent='';
+  document.getElementById("demoA").style.display="hidden";
+  var table = document.createElement("table"), row, cellA, cellB;
+  document.getElementById("demoA").appendChild(table);
+  var table2 = document.createElement("table"), row, cellA, cellB;
+  document.getElementById("demoB").appendChild(table2);
+  insertCells(table, data);
+  insertCells(table2, data);
+}
+
+function insertCells(table, data){
+  rowheader=table.insertRow();
+  row = table.insertRow();
+  row2 = table.insertRow();
+  row3 = table.insertRow();
+  row4 = table.insertRow();
+  var iconurl =  'http://openweathermap.org/img/wn/'+data.iconCode+'.png';
+  var img = document.createElement('img');
+  img.src = iconurl;
+  rowheader.insertCell().innerHTML=data.location;
+  rowheader.insertCell().appendChild(img);
+  row.insertCell().innerHTML='Temperatur';
+  row3.insertCell().innerHTML='Windrichtung';
+  row4.insertCell().innerHTML='Windgeschwindigkeit';
+  row.insertCell().innerHTML=data.temperature;
+  row3.insertCell().innerHTML=data.windDirection;
+  row4.insertCell().innerHTML=data.windSpeed;
+}
+
+
+
+
+
 
 
 
